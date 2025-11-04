be9da97c8b09c16723ef94c9de18e2ab440083c6
Justus Adam <dev@justus.science>
2025-11-04 11:01:33 -0500

    HW7 done

--- LoVe/Homework/Homework7.lean
+++ LoVe/Homework/Homework7.lean
@@ -51,6 +51,9 @@ undirected! -/
 inductive Quad : Type
 | one | two | three | four
 
+def Quad.all : List Quad :=
+  [one, two, three, four]
+
 -- Feel free to ignore these lines
 deriving instance Repr for ListGraph
 deriving instance Repr for Quad
@@ -58,11 +61,24 @@ deriving instance Repr for Quad
 section
 open Quad
 
-def K₄_quad_lg : ListGraph Quad :=
-  sorry
+deriving instance DecidableEq for Quad
 
-def K₄_quad_pg : PredGraph Quad :=
-  sorry
+def K₄_quad_lg : ListGraph Quad where
+  edges := do
+    let x <- Quad.all
+    let y <- Quad.all
+    if x = y then [] else return (x, y)
+
+def K₄_quad_pg : PredGraph Quad where
+  adj := fun x y => x ≠ y
+  symm := by
+    unfold Symmetric
+    intros x y h
+    apply Ne.symm
+    assumption
+  loopless := by
+    unfold Irreflexive
+    simp
 
 end
 
@@ -95,8 +111,18 @@ mathematically as ℤ/5ℤ. -/
 with a hypothesis `h : 0 = 1` or something like that, `cases h` may come in
 handy. -/
 
-def C₅ : PredGraph (Fin 5) :=
-  sorry
+def C₅ : PredGraph (Fin 5) where
+  adj x y := y = (x + 1) ∨ x = y + 1
+  symm := by
+    unfold Symmetric
+    intros x y h
+    cases h
+    (expose_names; exact Or.inr h)
+    (expose_names; exact Or.inl h)
+  loopless := by
+    unfold Irreflexive
+    intros x
+    simp
 
 /- ### 1.3 (1 point).
 
@@ -104,8 +130,18 @@ Using this functional definition of a graph, we can easily
 generalize the above cycle graph to make an arbitrarily large cycle. Fill in the
 following definition, mirroring your previous definition. -/
 
-def C₁₀₀ : PredGraph (Fin 100) :=
-  sorry
+def C₁₀₀ : PredGraph (Fin 100) where
+  adj x y := y = (x + 1) ∨ x = y + 1
+  symm := by
+    unfold Symmetric
+    intros x y h
+    cases h
+    (expose_names; exact Or.inr h)
+    (expose_names; exact Or.inl h)
+  loopless := by
+    unfold Irreflexive
+    intros x
+    simp
 
 /- Food for thought: how could we define `C₁₀₀` as a `ListGraph` without typing
 out 100 tuples? -/
@@ -193,11 +229,13 @@ and flips all the rest (convince yourself that all flips are of this form!).
 Hint: we strongly suggest you express both functions as closed-form formulas.
 Try to make your formulas as simple as possible. -/
 
-def C₅_rotate (n : Fin 5) : Fin 5 → Fin 5 :=
-  sorry
+def C₅_rotate (n : Fin 5) : Fin 5 → Fin 5 := (. + n)
+
+example {y n : ℤ} : (0 - (y - n)) + n = 2 * n - y := by
+  linarith
 
 def C₅_flip (n : Fin 5) : Fin 5 → Fin 5 :=
-  sorry
+  fun y => n + n - y
 
 /- ### 2α.2 (2 points).
 
@@ -208,12 +246,71 @@ your proofs using tactics like `simp`, see if you can simplify your definitions
 of the automorphisms above. -/
 
 @[autogradedProof 1]
-theorem C₅_rotate_is_aut : ∀ n, IsGraphAutomorphism C₅ (C₅_rotate n) :=
-  sorry
+theorem C₅_rotate_is_aut : ∀ n, IsGraphAutomorphism C₅ (C₅_rotate n) := by
+  intros n
+  unfold IsGraphAutomorphism
+  intros v₁ v₂
+  unfold C₅
+  simp
+  unfold C₅_rotate
+  constructor
+  . intros h
+    apply h.imp
+    . intros hₗ
+      rw [hₗ]
+      exact add_right_comm v₁ 1 n
+    . intros hᵣ
+      rw [hᵣ]
+      exact add_right_comm v₂ 1 n
+  . intros h
+    apply h.imp
+    . intros hₗ
+      rw [add_right_comm] at hₗ
+      exact add_right_cancel hₗ
+      done
+    . intros hᵣ
+      rw [add_right_comm] at hᵣ
+      exact add_right_cancel hᵣ
+      done
+
 
 @[autogradedProof 1]
-theorem C₅_flip_is_aut : ∀ n, IsGraphAutomorphism C₅ (C₅_flip n) :=
-  sorry
+theorem C₅_flip_is_aut : ∀ n, IsGraphAutomorphism C₅ (C₅_flip n) := by
+  intros n
+  unfold IsGraphAutomorphism C₅ C₅_flip
+  simp
+  intros v₁ v₂
+  constructor
+  . intros h
+    cases h with
+    | inl h =>
+      rw [h]
+      rw [add_sub_assoc, add_sub_assoc, add_assoc, add_assoc]
+      rw [add_left_cancel_iff, add_left_cancel_iff]
+      rw [sub_add, sub_add, sub_right_inj, add_sub_cancel_right]
+      right
+      rfl
+    | inr h =>
+      rw [h]
+      rw [sub_add, sub_add]
+      rw [sub_right_inj, sub_right_inj]
+      left
+      rw [add_sub_cancel_right]
+      done
+  . intros h
+    cases h with
+    | inl h_r =>
+      simp [sub_add] at h_r
+      right
+      symm
+      exact add_eq_of_eq_sub h_r
+      done
+    | inr h_r =>
+      simp [sub_add] at h_r
+      left
+      symm
+      exact add_eq_of_eq_sub h_r
+      done
 
 /- Now we can define the elements of our structure! -/
 def C₅_rotate_aut (n : Fin 5) : GraphAutomorphism (Fin 5) C₅ := {
@@ -242,12 +339,20 @@ Hint for the lemma: `Iff.trans` might be handy!
 lemma aut_comp_aut {α : Type} (G : PredGraph α) (f g : α → α) :
   IsGraphAutomorphism G f →
   IsGraphAutomorphism G g →
-  IsGraphAutomorphism G (f ∘ g) :=
-  sorry
+  IsGraphAutomorphism G (f ∘ g) := by
+  unfold IsGraphAutomorphism
+  intros h1 h2 v1 v2
+  apply Iff.trans
+  exact h1 v1 v2
+  exact (iff_congr (h1 v1 v2) (h1 (g v1) (g v2))).mp (h2 v1 v2)
 
 def aut_comp {α : Type} (G : PredGraph α) :
- GraphAutomorphism α G → GraphAutomorphism α G → GraphAutomorphism α G :=
-  sorry
+ GraphAutomorphism α G → GraphAutomorphism α G → GraphAutomorphism α G := by
+  intros g1 g2
+  constructor
+  swap
+  exact g1.f ∘ g2.f
+  exact aut_comp_aut G g1.f g2.f g1.is_aut g2.is_aut
 
 -- We define convenience notation for the composition operation on the
 -- automorphism group of `path₅`
@@ -263,14 +368,24 @@ arbitrary rotation? -/
 
 @[autogradedProof 1]
 lemma GraphAutomorphism.assoc :
-  ∀ (a b c : GraphAutomorphism (Fin 5) C₅), a ∘₅ b ∘₅ c = a ∘₅ (b ∘₅ c) :=
-  sorry
+  ∀ (a b c : GraphAutomorphism (Fin 5) C₅), a ∘₅ b ∘₅ c = a ∘₅ (b ∘₅ c) := by
+  intros a b c
+  unfold aut_comp
+  rfl
+  done
 
 -- Note: You **do not** need to provide the `is_aut` field for the value you
 -- return; feel free to `sorry` that proof.
 def GraphAutomorphism.inv :
-  GraphAutomorphism (Fin 5) C₅ → GraphAutomorphism (Fin 5) C₅ :=
+  GraphAutomorphism (Fin 5) C₅ → GraphAutomorphism (Fin 5) C₅ := by
+  intros a
+  constructor
   sorry
+  let f := a.f
+  exact f ∘ f ∘ f ∘ f
+  done
+
+
 
 /- You don't have to prove the rest, but if you did, you'd have a group! (If
 you've studied some group theory, see if you can identify which group this is.)
@@ -316,7 +431,10 @@ Fill in the predicate `IsPath` such that `IsPath G v₁ v₂ vs` holds just when
 `vs` is a valid path from `v₁` to `v₂` in `G`. -/
 
 inductive IsPath {α : Type} : PredGraph α → α → α → List α → Prop
--- Fill this in!
+| step {G mid next endV vs} :
+  (adj: G.adj mid next) → (rest: IsPath G next endV (next::vs))
+    → IsPath G mid endV (mid :: next :: vs)
+| fin {G endV} : IsPath G endV endV [endV]
 
 -- A graph is connected if there is a path between any two distinct vertices
 def IsConnected {α : Type} (G : PredGraph α) : Prop :=
@@ -339,6 +457,8 @@ totally disjoint (i.e., they have no elements in common).
 Hint: you may find the predicate `x ∈ xs` useful! -/
 
 inductive ListDisj {α : Type} : List α → List α → Prop
+| empty {vs} : ListDisj [] vs
+| cons {x xs vs} : (x ∉ vs) → ListDisj (x::xs) vs
 -- Fill this in!
 
 /- ### 2β.3 (2 points).
@@ -346,7 +466,7 @@ inductive ListDisj {α : Type} : List α → List α → Prop
 Use the predicate above to write a definition for biconnectivity. -/
 
 def IsBiconnected {α : Type} (G : PredGraph α) : Prop :=
-  sorry
+  ∀ (x y : α), x ≠ y → ∃ (p₁ p₂ : List α), IsPath G x y (x :: p₁ ++ [y]) ∧ IsPath G x y (x :: p₂ ++ [y]) ∧ ListDisj p₁ p₂
 
 /- ### 2β.4 (2 points).
 
@@ -386,7 +506,24 @@ lemma exist_distinct_fin4_of_neq :
   | ⟨3, _⟩, ⟨3, _⟩, h => absurd rfl h
 
 @[autogradedProof 2]
-theorem K₄_fin_is_biconnected : IsBiconnected K₄_fin :=
-  sorry
+theorem K₄_fin_is_biconnected : IsBiconnected K₄_fin := by
+  unfold IsBiconnected
+  intros x y h_eq
+  have ⟨ a, b, h_neqs ⟩ := exist_distinct_fin4_of_neq x y h_eq
+  have ⟨ x_neq_a, a_neq_y, x_neq_b, b_neq_y, a_neq_b ⟩ := h_neqs
+  apply Exists.intro
+  swap ; exact [a]
+  apply Exists.intro
+  swap ; exact [b]
+  constructor
+  repeat (constructor ; unfold K₄_fin ; simp ; assumption)
+  constructor
+  constructor
+  repeat (constructor ; unfold K₄_fin ; simp ; assumption)
+  constructor
+  constructor
+  simp
+  assumption
+  done
 
 end LoVe
